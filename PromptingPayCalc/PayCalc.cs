fd13484e6e97d915f218a6f20d91ef3a058f0088
Curtis Hansen <curtishansen14@gmail.com>
2017-12-19 13:20:00 -0600

    Csharp pay Calculator

---
+++ PromptingPayCalc/PayCalc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromptingPayCalc
+{
+    class PayCalc
+    {
+
+        public string promptForHours()
+        {
+            string hoursInput;
+            Console.WriteLine("Please enter your hours");
+            hoursInput = Console.ReadLine();
+            return hoursInput;
+        }
+
+        public string promptForWage()
+        {
+            string wageInput;
+            Console.WriteLine("Please enter your wage");
+            wageInput = Console.ReadLine();
+            return wageInput;
+        }
+
+        public float Calculator(string hoursInput, string wageInput)
+        {
+             float hours = float.Parse(hoursInput);
+             float wage = float.Parse(wageInput);
+
+            float pay = hours * wage;
+            
+            Console.WriteLine("Your pay is {0}", pay);
+            Console.ReadKey();
+            
+            return pay; 
+           
+        }    
+
+    }
+}
---
+++ PromptingPayCalc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromptingPayCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+              PayCalc pay = new PayCalc();
+                string inputHours = pay.promptForHours();
+                string inputWage = pay.promptForWage();
+                pay.Calculator(inputHours, inputWage);
+                Console.ReadKey();
+
+        }
+    }
+}
